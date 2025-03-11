4e738ebb97f6117139c758aeec183d07d32f762a
KeaMeng <2023484ing@aupp.edu.kh>
2025-03-11 22:29:38 +0700

    database config

--- db/pool.js
+++ db/pool.js
@@ -4,7 +4,13 @@ require("dotenv").config(); // Load .env variables
 
 module.exports = new Pool({
   connectionString: process.env.DATABASE_URL,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
+
   ssl: {
-    rejectUnauthorized: false, // Required for Railway's PostgreSQL
+    rejectUnauthorized: false, // Required for Render.com PostgreSQL
   },
 });
